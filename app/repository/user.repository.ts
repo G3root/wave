582adac162008a967aadf2e187a7c48d2374e253
nafees nazik <nafeesnazik21@gmail.com>
2024-01-01 17:05:51 +0530

    feat: add repoistory

---
+++ app/repository/user.repository.ts
@@ -0,0 +1,51 @@
+import { type KyselyDb } from '~/utils/db.server'
+import { generatePublicId } from '~/utils/public-id'
+
+interface getUserOption {
+	column: 'publicId' | 'email'
+	value: string
+}
+
+export const getUser = (db: KyselyDb, { column, value }: getUserOption) => {
+	return db
+		.selectFrom('user')
+		.where(column, '=', value)
+		.selectAll()
+		.executeTakeFirst()
+}
+
+interface createUserOption {
+	email: string
+	name?: string
+}
+
+export const createUser = async (
+	db: KyselyDb,
+	{ email, name }: createUserOption,
+) => {
+	const data = await db
+		.insertInto('user')
+		.values({
+			publicId: generatePublicId('user'),
+			email,
+		})
+		.returningAll()
+		.executeTakeFirst()
+	return data
+}
+
+interface getUsersFirstWorkspaceOptions {
+	userPublicId: string
+}
+
+export const getUsersFirstWorkspace = async (
+	db: KyselyDb,
+	{ userPublicId }: getUsersFirstWorkspaceOptions,
+) => {
+	return db
+		.selectFrom('workspace as ws')
+		.innerJoin('membership as p', 'p.workspaceId', 'ws.publicId')
+		.where('p.userId', '=', userPublicId)
+		.selectAll('ws')
+		.executeTakeFirst()
+}
---
+++ app/repository/workspace.repository.server.ts
@@ -0,0 +1,50 @@
+import {
+	type Config,
+	uniqueNamesGenerator,
+	adjectives,
+	colors,
+} from 'unique-names-generator'
+import { type KyselyDb } from '~/utils/db.server'
+
+import { generatePublicId } from '~/utils/public-id'
+
+const config: Config = {
+	dictionaries: [adjectives, colors],
+	separator: '-',
+}
+
+interface createWorkspaceOptions {
+	workspaceName?: string | null | undefined
+	userPublicId: string
+}
+
+export const createWorkspace = async (
+	db: KyselyDb,
+	{ userPublicId, workspaceName: workspaceName_ }: createWorkspaceOptions,
+) => {
+	const workspaceName = workspaceName_ ?? uniqueNamesGenerator(config)
+
+	const workspace = await db
+		.insertInto('workspace')
+		.values({
+			name: workspaceName,
+			publicId: generatePublicId('workspace'),
+			creatorId: userPublicId,
+		})
+		.returningAll()
+		.executeTakeFirstOrThrow()
+
+	const membership = await db
+		.insertInto('membership')
+		.values({
+			publicId: generatePublicId('member'),
+			workspaceId: workspace.publicId,
+			status: 'accepted',
+			joinedAt: new Date().toISOString(),
+			userId: userPublicId,
+		})
+		.returningAll()
+		.executeTakeFirstOrThrow()
+
+	return { workspace, membership }
+}
