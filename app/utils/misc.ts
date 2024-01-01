7cfc0e716e5053b6b8b5b9d295bf4c70e15c765c
nafees nazik <nafeesnazik21@gmail.com>
2024-01-01 22:39:46 +0530

    feat: add error boundary component

--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -4,3 +4,33 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+/**
+ * Combine multiple header objects into one (uses append so headers are not overridden)
+ */
+export function combineHeaders(
+	...headers: Array<ResponseInit['headers'] | null | undefined>
+) {
+	const combined = new Headers()
+	for (const header of headers) {
+		if (!header) continue
+		for (const [key, value] of new Headers(header).entries()) {
+			combined.append(key, value)
+		}
+	}
+	return combined
+}
+
+export function getErrorMessage(error: unknown) {
+	if (typeof error === 'string') return error
+	if (
+		error &&
+		typeof error === 'object' &&
+		'message' in error &&
+		typeof error.message === 'string'
+	) {
+		return error.message
+	}
+	console.error('Unable to get error message for error', error)
+	return 'Unknown Error'
+}
