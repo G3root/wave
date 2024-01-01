108d4e3658769a9513e36d2efbd91b2d5a2207fb
nafees nazik <nafeesnazik21@gmail.com>
2024-01-02 01:29:05 +0530

    fix: padding

--- app/routes/_dashboard/sidebar/sidebar.tsx
+++ app/routes/_dashboard/sidebar/sidebar.tsx
@@ -68,7 +68,7 @@ export function SideBar({ className }: SideBarProps) {
 		<div className={cn('pb-12', className)}>
 			<div className="gap-y-4 py-4">
 				<div className="flex flex-col gap-y-4 px-3 py-2">
-					<div className="gap-y-1">
+					<div className="flex flex-col gap-y-1">
 						{navigation.map(item => (
 							<NavItem key={item.label} {...item} />
 						))}
