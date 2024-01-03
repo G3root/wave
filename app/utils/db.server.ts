ca8d03c3b95572cdd072952babe371a0ba18a396
nafees nazik <nafeesnazik21@gmail.com>
2024-01-03 18:20:38 +0530

    feat: add remember singleton for db

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,14 +1,19 @@
+import { remember } from '@epic-web/remember'
 import SQLite from 'better-sqlite3'
 import { CamelCasePlugin, Kysely, SqliteDialect } from 'kysely'
-import { type DB } from '~/types/db' // this is the Database interface we defined earlier
+import { type DB } from '~/types/db'
 
 const dialect = new SqliteDialect({
 	database: new SQLite('data.db'),
 })
 
-export const db = new Kysely<DB>({
-	dialect,
-	plugins: [new CamelCasePlugin()],
-})
+export const db = remember(
+	'kysely',
+	() =>
+		new Kysely<DB>({
+			dialect,
+			plugins: [new CamelCasePlugin()],
+		}),
+)
 
 export type KyselyDb = typeof db
