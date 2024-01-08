be16607b73fe8614eea9bb19d140a2e341857c48
nafees nazik <nafeesnazik21@gmail.com>
2024-01-08 23:53:18 +0530

    feat: add new queries

--- app/repository/project.repository.server.ts
+++ app/repository/project.repository.server.ts
@@ -23,3 +23,30 @@ export const getWorkspaceProjects = (
 		.where('workspaceId', '=', workspaceId)
 		.execute()
 }
+
+export const getMemberProjects = (
+	db: KyselyDb,
+	{ membershipId, workspaceId }: { membershipId: string; workspaceId: string },
+) => {
+	return db
+		.selectFrom('project as p')
+		.innerJoin('projectOnMembers as pm', 'pm.projectId', 'p.publicId')
+		.where('p.workspaceId', '==', workspaceId)
+		.where('pm.membershipId', '==', membershipId)
+		.selectAll('p')
+		.execute()
+}
+
+export const getMemberProject = (
+	db: KyselyDb,
+	{ membershipId, projectId }: { membershipId: string; projectId: string },
+) => {
+	return db
+		.selectFrom('project as p')
+		.innerJoin('projectOnMembers as pm', 'pm.projectId', 'p.publicId')
+		.where('pm.projectId', '==', projectId)
+		.where('pm.membershipId', '==', membershipId)
+		.limit(1)
+		.selectAll('p')
+		.executeTakeFirst()
+}
