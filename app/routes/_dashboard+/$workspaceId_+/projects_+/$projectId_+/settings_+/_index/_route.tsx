0b66f8a1929ecafc94750f8a189e75123171d5df
nafees nazik <nafeesnazik21@gmail.com>
2024-01-08 21:24:47 +0530

    feat: add projects page

--- app/routes/_dashboard+/$workspaceId_+/projects_+/$projectId_+/settings_+/_index/_route.tsx
+++ app/routes/_dashboard+/$workspaceId_+/projects_+/$projectId_+/settings_+/_index/_route.tsx
@@ -1,3 +1,21 @@
+import { type LoaderFunctionArgs, json } from '@remix-run/node'
+import * as z from 'zod'
+import { auth } from '~/utils/auth.server'
+
+const paramsSchema = z.object({
+	workspaceId: z.string(),
+	projectId: z.string(),
+})
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+	await auth.isAuthenticated(request, {
+		failureRedirect: '/login',
+	})
+	const { projectId, workspaceId } = paramsSchema.parse(params)
+
+	return json({ projectId, workspaceId })
+}
+
 export default function ProjectSettingsPage() {
 	return <div>Project settings Page</div>
 }
