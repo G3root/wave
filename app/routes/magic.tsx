8ae9abcb31dc5f114689917f87fa097736f04f45
nafees nazik <nafeesnazik21@gmail.com>
2024-01-10 08:34:59 +0530

    fix: redirect

--- app/routes/magic.tsx
+++ app/routes/magic.tsx
@@ -7,7 +7,7 @@ export let loader = async ({ request }: LoaderFunctionArgs) => {
 		// This redirect is optional, if not defined the user will be returned by
 		// the `authenticate` function and you can render something on this page
 		// manually redirect the user.
-		successRedirect: '/me',
+		successRedirect: '/',
 		// If something failed we take them back to the login page
 		// This redirect is optional, if not defined any error will be throw and
 		// the ErrorBoundary will be rendered.
